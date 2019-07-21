a90abc33f68a93116e114ae92c6f954742d33097
Jessica <martijnvermeulen1@gmail.com>
2019-07-21 19:33:32 +0200

    commit19332107

--- scripts/query_db.js
+++ scripts/query_db.js
@@ -7,21 +7,28 @@
 		}
 	
 		DataBase.prototype.queryDb = function (filiaalnummer) {
-		this.dbRef.orderByChild("filiaalnummer").equalTo(Number(filiaalnummer)).once("value", function(snapshot) {
-			if (!snapshot.exists()) { 
-				alert("filiaalnummer niet gevonden")
-				return
-			}
+			return this.dbRef.orderByChild("filiaalnummer").equalTo(Number(filiaalnummer)).once("value").then(function(snapshot) {
+				if (!snapshot.exists()) { 
+					alert("filiaalnummer niet gevonden")
+					return
+				}
+					return snapshot
+			})
+		}
+		
+
+		DataBase.prototype.updateElementFromDb = function (htmlElement,filiaalnummer) {
+			this.queryDb(filiaalnummer).then(function (snapshot) {
 			snapshot.forEach(function(element) {
 				var dataList = element.val()
 				var div = $("<div></div>",{class: "queryText"} )
 				div.html("<span>" +  "<b>Filiaalnummer:</b> " + dataList.filiaalnummer + "</span><br><br>" +
-						"<span>" + "<b>Adres:</b> " + dataList.address + "</span><br><br>" +
+						"<a class='maps-link' href='https://www.google.com/maps?q=" + dataList.address + ",netherlands'><span>" + "<b>Adres:</b> " + dataList.address + "</span></a><br><br>" +
 						"<span>" + "<b>Postcode:</b> " + dataList.postcode + "</span><br><br>" +
 						"<span class=\"info-text\">" + "<b>info:</b> " + dataList.info + "</span><br><br>"
 				)
 	
-				$(".main-ui").append(div)	
+				$(htmlElement).append(div)	
 				if(dataList.mededeling) $(".info-text").before("<span>" + "<b>Mededeling:</b> " + dataList.mededeling + "</span><br><br>") 
 				
 			})
@@ -38,12 +45,13 @@
 						if (dataList.mededeling) {
 							var listElement = $("<li></li>")
 							listElement.html("<span>" + "Filiaalnummer: " + dataList.filiaalnummer + "</span><br><br>" +
-								"<span>" + "Adres: " + dataList.address + "</span><br><br>" +
+								"<a class='maps-link' href='https://www.google.com/maps?q=" + dataList.address + ",netherlands'><span>" + "Adres: " + dataList.address + "</span></a><br><br>" +
 								"<span>" + "Mededeling: " + dataList.mededeling + "</span><br><br>"
 							)
 							listElement.css("color","white")
 							listElement.css("background", "grey")
-							listElement.css("margin", "20px")
+							listElement.css("padding", "10px")
+							listElement.css("margin","20px")
 							$(".mededelingen-list").append(listElement)
 						}
 					})
@@ -51,15 +59,8 @@
 			})
 	}
 					
-					
-	function clearTextNode() {
-		"use strict"
-		var element = $(".queryText")
-		element.remove()
-	}
 
 	App.DataBase = DataBase
-	App.clearTextNode = clearTextNode
 	window.App = App
 })(window)
 
