2c9a9798140af7e3ca6831fcb22fa08e9bd67d25
Jessica <martijnvermeulen1@gmail.com>
2019-07-21 15:17:33 +0200

    commit 15172107

---
+++ scripts/initialize_fb.js
@@ -0,0 +1,13 @@
+
+	// Your web app's Firebase configuration
+	var firebaseConfig = {
+		apiKey: "AIzaSyDF2m4vvAS0zQXfn2NQgQxrDZGRblq0wY8",
+		authDomain: "tielbeke3.firebaseapp.com",
+		databaseURL: "https://tielbeke3.firebaseio.com",
+		projectId: "tielbeke3",
+		storageBucket: "tielbeke3.appspot.com",
+		messagingSenderId: "1010883935820",
+		appId: "1:1010883935820:web:3320f49198950cf0"
+	};
+	// Initialize Firebase
+	firebase.initializeApp(firebaseConfig);
--- scripts/query_db.js
+++ scripts/query_db.js
@@ -9,25 +9,49 @@ function queryDB(filiaalnummer) {
 		}
 		snapshot.forEach(function(element) {
 			var dataList = element.val()
-			var div = document.createElement("div")
-			div.classList.add("queryText")
-			div.innerHTML = "<span>" + "filiaalnummer: " + dataList.filiaalnummer + "</span><br><br>" +
-							"<span>" + "adres: " + dataList.address + "</span><br><br>" +
-							"<span>" + "postcode: " + dataList.postcode + "</span><br><br>" +
-							"<span>" + "info :" + dataList.info + "</span><br><br>"
+			var div = $("<div></div>",{class: "queryText"} )
+			div.html("<span>" +  "<b>Filiaalnummer:</b> " + dataList.filiaalnummer + "</span><br><br>" +
+					 "<span>" + "<b>Adres:</b> " + dataList.address + "</span><br><br>" +
+					 "<span>" + "<b>Postcode:</b> " + dataList.postcode + "</span><br><br>" +
+					 "<span class=\"Info-text\">" + "<b>info:</b> " + dataList.info +"<br><br>-------------------------------------------------------------" + "</span><br><br>"
+			)
+
+			$(".main-ui").append(div)	
+			if(dataList.mededeling) $(".info-text").before("<span>" + "<b>Mededeling:</b> " + dataList.mededeling + "</span><br><br>") 
 			
-			document.getElementsByClassName("main-ui")[0].appendChild(div)
 		})
 		
 	})
 		
 }
 
+function getAllMededelingen() {
+	"use strict"
+		var dbRef = firebase.database().ref()
+		dbRef.orderByChild("filiaalnummer").once("value", function(snapshot) {
+			if(snapshot.exists()) {
+				snapshot.forEach(function(element) {
+					var dataList = element.val()
+					if (dataList.mededeling) {
+						var listElement = $("<li></li>")
+						listElement.html("<span>" + "Filiaalnummer: " + dataList.filiaalnummer + "</span><br><br>" +
+							"<span>" + "Adres: " + dataList.address + "</span><br><br>" +
+							"<span>" + "Mededeling: " + dataList.mededeling + "</span><br><br>"
+						)
+						listElement.css("color","white")
+						listElement.css("background", "grey")
+						listElement.css("margin", "20px")
+						$(".mededelingen-list").append(listElement)
+					}
+				})
+			}
+		})
+}
+				
+				
 function clearTextNode() {
 	"use strict"
-	var textNode = document.querySelectorAll("[class=\"queryText\"]")
-	var textNodeArray = [].slice.call(textNode)
-	textNodeArray.forEach(function(element) {
-		element.parentNode.removeChild(element)
-	})
+	var element = $(".queryText")
+	element.children().remove()
 }
+
